6b303c48765d3fe1b4462a8b34d4abe62ad93e83
unknown <taha.iu.bd@gmail.com>
2022-04-30 20:05:37 +0600

    invontery-card-degin

--- src/components/FourItems/FourItems.js
+++ src/components/FourItems/FourItems.js
@@ -3,22 +3,23 @@ import { Link } from 'react-router-dom'
 
 const FourItems = () => {
     return (
-        <div className="md:h-[100vh] flex flex-col justify-center">
+        <div className="md:min-h-[110vh] flex flex-col justify-center">
             <div className="mx-auto">
                 <h1 className="text-6xl font-bold 2xl:leading-10 leading-0 text-center text-gray-800">
                     Inventories
                 </h1>
             </div>
-            <div className="grid mx-10 gap-10 grid-cols-1 md:pt-28 md:grid-cols-2 max-w-7xl md:mx-auto">
-                <div className="flex max-w-md mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800">
-                    <div className="w-1/3 bg-cover">
+            <div className="grid mx-10 mt-10 gap-10 grid-cols-1 md:pt-28 md:grid-cols-2 max-w-7xl md:mx-auto">
+                <div className="md:flex max-w-md mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800">
+                    <div className="md:w-1/3  bg-cover">
                         <img
-                            src="https://i.ibb.co/YXdGsz7/Untitled-design-7.png"
+                            className=" md:h-full h-[270px] w-full"
+                            src="https://i.ibb.co/WcsFPnz/Untitled-design-11.png"
                             alt=""
                         />
                     </div>
 
-                    <div className="w-2/3 p-4 md:p-4">
+                    <div className="md:w-2/3 w-full p-4 md:p-4">
                         <h1 className="text-2xl font-bold text-gray-800 dark:text-white">
                             Orange
                         </h1>
@@ -30,28 +31,29 @@ const FourItems = () => {
                         </p>
                         <p className="mt-5">Avilable: 500 carot</p>
 
-                        <div className="flex justify-between mt-3 item-center">
+                        <div className="md:flex justify-between mt-3 item-center">
                             <h1 className="text-lg font-bold text-gray-700 dark:text-gray-200 md:text-xl">
                                 $220{' '}
                                 <span className="text-sm font-normal text-gray-00 ">
                                     ( par carot )
                                 </span>
                             </h1>
-                            <button className="px-2 py-1 text-xs font-bold text-white uppercase transition-colors duration-200 transform bg-gray-800 rounded dark:bg-gray-700 hover:bg-gray-700 dark:hover:bg-gray-600 focus:outline-none focus:bg-gray-700 dark:focus:bg-gray-600">
+                            <button className="px-2 py-1 mt-2 text-xs font-bold text-white uppercase transition-colors duration-200 transform bg-gray-800 rounded dark:bg-gray-700 hover:bg-gray-700 dark:hover:bg-gray-600 focus:outline-none focus:bg-gray-700 ">
                                 Update
                             </button>
                         </div>
                     </div>
                 </div>
-                <div className="flex max-w-md mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800">
-                    <div className="w-1/3 bg-cover">
+                <div className="md:flex max-w-md mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800">
+                    <div className="md:w-1/3  bg-cover">
                         <img
-                            src="https://i.ibb.co/YXdGsz7/Untitled-design-7.png"
+                            className=" md:h-full h-[270px] w-full"
+                            src="https://i.ibb.co/WcsFPnz/Untitled-design-11.png"
                             alt=""
                         />
                     </div>
 
-                    <div className="w-2/3 p-4 md:p-4">
+                    <div className="md:w-2/3 w-full p-4 md:p-4">
                         <h1 className="text-2xl font-bold text-gray-800 dark:text-white">
                             Orange
                         </h1>
@@ -63,28 +65,29 @@ const FourItems = () => {
                         </p>
                         <p className="mt-5">Avilable: 500 carot</p>
 
-                        <div className="flex justify-between mt-3 item-center">
+                        <div className="md:flex justify-between mt-3 item-center">
                             <h1 className="text-lg font-bold text-gray-700 dark:text-gray-200 md:text-xl">
                                 $220{' '}
                                 <span className="text-sm font-normal text-gray-00 ">
                                     ( par carot )
                                 </span>
                             </h1>
-                            <button className="px-2 py-1 text-xs font-bold text-white uppercase transition-colors duration-200 transform bg-gray-800 rounded dark:bg-gray-700 hover:bg-gray-700 dark:hover:bg-gray-600 focus:outline-none focus:bg-gray-700 dark:focus:bg-gray-600">
+                            <button className="px-2 py-1 mt-2 text-xs font-bold text-white uppercase transition-colors duration-200 transform bg-gray-800 rounded dark:bg-gray-700 hover:bg-gray-700 dark:hover:bg-gray-600 focus:outline-none focus:bg-gray-700 ">
                                 Update
                             </button>
                         </div>
                     </div>
                 </div>
-                <div className="flex max-w-md mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800">
-                    <div className="w-1/3 bg-cover">
+                <div className="md:flex max-w-md mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800">
+                    <div className="md:w-1/3  bg-cover">
                         <img
-                            src="https://i.ibb.co/YXdGsz7/Untitled-design-7.png"
+                            className=" md:h-full h-[270px] w-full"
+                            src="https://i.ibb.co/WcsFPnz/Untitled-design-11.png"
                             alt=""
                         />
                     </div>
 
-                    <div className="w-2/3 p-4 md:p-4">
+                    <div className="md:w-2/3 w-full p-4 md:p-4">
                         <h1 className="text-2xl font-bold text-gray-800 dark:text-white">
                             Orange
                         </h1>
@@ -96,28 +99,29 @@ const FourItems = () => {
                         </p>
                         <p className="mt-5">Avilable: 500 carot</p>
 
-                        <div className="flex justify-between mt-3 item-center">
+                        <div className="md:flex justify-between mt-3 item-center">
                             <h1 className="text-lg font-bold text-gray-700 dark:text-gray-200 md:text-xl">
                                 $220{' '}
                                 <span className="text-sm font-normal text-gray-00 ">
                                     ( par carot )
                                 </span>
                             </h1>
-                            <button className="px-2 py-1 text-xs font-bold text-white uppercase transition-colors duration-200 transform bg-gray-800 rounded dark:bg-gray-700 hover:bg-gray-700 dark:hover:bg-gray-600 focus:outline-none focus:bg-gray-700 dark:focus:bg-gray-600">
+                            <button className="px-2 py-1 mt-2 text-xs font-bold text-white uppercase transition-colors duration-200 transform bg-gray-800 rounded dark:bg-gray-700 hover:bg-gray-700 dark:hover:bg-gray-600 focus:outline-none focus:bg-gray-700 ">
                                 Update
                             </button>
                         </div>
                     </div>
                 </div>
-                <div className="flex max-w-md mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800">
-                    <div className="w-1/3 bg-cover">
+                <div className="md:flex max-w-md mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800">
+                    <div className="md:w-1/3  bg-cover">
                         <img
-                            src="https://i.ibb.co/YXdGsz7/Untitled-design-7.png"
+                            className=" md:h-full h-[270px] w-full"
+                            src="https://i.ibb.co/WcsFPnz/Untitled-design-11.png"
                             alt=""
                         />
                     </div>
 
-                    <div className="w-2/3 p-4 md:p-4">
+                    <div className="md:w-2/3 w-full p-4 md:p-4">
                         <h1 className="text-2xl font-bold text-gray-800 dark:text-white">
                             Orange
                         </h1>
@@ -129,14 +133,14 @@ const FourItems = () => {
                         </p>
                         <p className="mt-5">Avilable: 500 carot</p>
 
-                        <div className="flex justify-between mt-3 item-center">
+                        <div className="md:flex justify-between mt-3 item-center">
                             <h1 className="text-lg font-bold text-gray-700 dark:text-gray-200 md:text-xl">
                                 $220{' '}
                                 <span className="text-sm font-normal text-gray-00 ">
                                     ( par carot )
                                 </span>
                             </h1>
-                            <button className="px-2 py-1 text-xs font-bold text-white uppercase transition-colors duration-200 transform bg-gray-800 rounded dark:bg-gray-700 hover:bg-gray-700 dark:hover:bg-gray-600 focus:outline-none focus:bg-gray-700 dark:focus:bg-gray-600">
+                            <button className="px-2 py-1 mt-2 text-xs font-bold text-white uppercase transition-colors duration-200 transform bg-gray-800 rounded dark:bg-gray-700 hover:bg-gray-700 dark:hover:bg-gray-600 focus:outline-none focus:bg-gray-700 ">
                                 Update
                             </button>
                         </div>
