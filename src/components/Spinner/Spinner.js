8e2cc8183979d461eb7b7889cd3774d9044fbef9
unknown <taha.iu.bd@gmail.com>
2022-05-11 18:43:07 +0600

    loading

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Helmet } from 'react-helmet-async'
 
 const Spinner = () => {
     return (
-        <div className="h-full min-h-screen w-[100vw] bg-[#171C1D]">
+        <div className="h-full overflow-hidden min-h-screen w-[100vw] bg-[#171C1D]">
+            <Helmet>
+                <title>Loading . . . .</title>
+            </Helmet>
             <div className="flex align-center justify-center">
                 <img
                     className="h-[500px] w-[500px]  "
